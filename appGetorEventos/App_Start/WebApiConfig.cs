a0637bf1b9b530bcb95b789a9554d3a7ac62e627
Carlos Inga Principe <ceduardoinga@gmail.com>
2019-04-03 18:14:55 -0500

    Format json

--- appGetorEventos/App_Start/WebApiConfig.cs
+++ appGetorEventos/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace appGetorEventos
@@ -13,6 +14,10 @@ namespace appGetorEventos
             config.EnableCors();
             // Rutas de API web
             config.MapHttpAttributeRoutes();
+
+            //Formato JSON
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
